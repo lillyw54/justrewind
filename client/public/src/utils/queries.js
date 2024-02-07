ebe38c3009debed0e0d91a48cc6cd05fd37454b8
Joshua <Joshua.soto@live.com>
2024-02-06 19:24:01 -0600

    added mutations and queries

--- client/public/src/utils/queries.js
+++ client/public/src/utils/queries.js
@@ -1,31 +1,13 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      name
-      skills
+export const LOGIN_QUERY = gql`
+  query Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        id
+        email
+      }
     }
   }
 `;
